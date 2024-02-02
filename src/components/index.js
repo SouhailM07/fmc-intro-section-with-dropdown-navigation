b4721a52df071ae6215bdcacefecd5811332de7a
shadow <lightshadow416@gmail.com>
2024-02-02 21:23:17 +0100

    [Navbar_mobile] component was created

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as MyContainer } from "./MyContainer/MyContainer";
 export { default as Navbar } from "./Navbar/Navbar";
 export { default as Main } from "./Main/Main";
+export { default as Navbar_mobile } from "./Navbar_mobile/Navbar_mobile";
