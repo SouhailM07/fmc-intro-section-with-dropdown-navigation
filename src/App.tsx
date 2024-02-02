9df6a15fe962f74765589a51cdcca7e3a50b25f9
shadow <lightshadow416@gmail.com>
2024-02-02 19:19:50 +0100

    project core was built

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./style/input.css";
+import "./App.css";
+// components
+import { MyContainer } from "./components";
+
+function App() {
+  return (
+    <>
+      <MyContainer />
+    </>
+  );
+}
+export default App;
